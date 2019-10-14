b3beb99d4d0de3715df0c6cf549416f787aad8eb
Chris Kerr <chriskerr@me.com>
2019-10-14 15:56:09 +1100

    beginning break JS into function files

---
+++ js/ai.js
@@ -0,0 +1,9 @@
+function nextMove (aiDifficulty) {
+  // a function to suggest a next move, with accuracy based on aiDifficulty
+  // easy 45% correct
+  // normal 75% correct
+  // hard 100% correct
+
+  // min max theory?
+
+}
---
+++ js/evolution.js
@@ -0,0 +1,2 @@
+// a place to create the evolution animation to feed the gameover screen and potentially update the character set for new games
+// probably a newGame button too?
